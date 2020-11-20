6d24309dbc5443b5bd41845ef9b9fa75f6b4ee35
Gauthier Cassany <gauthier.cassany@gmail.com>
2020-11-20 06:02:31 +0000

    chores(deploy): change building workflow

--- OneDayOnePlant-Frontend/build.sh
+++ OneDayOnePlant-Frontend/build.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 rm -r build/
+docker build -t odop-front .
 docker create -it --name odop-build odop-front bash
 docker cp odop-build:/app/build ./build
 docker rm -f odop-build 
+docker image rm odop-front
 if [ -d "/var/www/onedayoneplant.net/html" ]
 then
 	rm -r /var/www/onedayoneplant.net/html/*
